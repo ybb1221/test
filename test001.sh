1573435eb00781ee8e33aa68be8a7ac1c7103031
ybb316860322 <ybb316860322@gmail.com>
2024-03-27 19:15:44 +0800

    fork

--- test001.sh
+++ test001.sh
@@ -1,24 +1,24 @@
-# #!/bin/bash
+#!/bin/bash
 
-# location=$HOME 
-# file_name="sentinel" 
-# # 
-# if [ -d $location ] 
-# then 
-#     echo "OK on the $location directory" 
-#     echo "Now checking on the file, $file_name..." 
-#     if [ -e $location/$file_name ] 
-#     then 
-#         echo "OK on the file, $file_name." 
-#         echo "Updating file's contents." 
-#         date >> $location/$file_name 
-#  # 
-#     else 
-#         echo "File, $location/$file_name, does NOT exist." 
-#         echo "Nothing to update." 
-#     fi 
-# # 
-# else
-#     echo "Directory, $location, does NOT exist." 
-#     echo "Nothing to update." 
-# fi
+location=$HOME 
+file_name="sentinel" 
+# 
+if [ -d $location ] 
+then 
+    echo "OK on the $location directory" 
+    echo "Now checking on the file, $file_name..." 
+    if [ -e $location/$file_name ] 
+    then 
+        echo "OK on the file, $file_name." 
+        echo "Updating file's contents." 
+        date >> $location/$file_name 
+ # 
+    else 
+        echo "File, $location/$file_name, does NOT exist." 
+        echo "Nothing to update." 
+    fi 
+# 
+else
+    echo "Directory, $location, does NOT exist." 
+    echo "Nothing to update." 
+fi
